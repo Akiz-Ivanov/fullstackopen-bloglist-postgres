f9cb05ff5f23e02275717a613a94e6447b0261d1
Akiz-Ivanov <ivanovicandreja@gmail.com>
2026-07-02 20:33:34 +0200

    feat(part13): exercises 5-7 - restructure app and add blog operations
    - Refactor flat index.js into MVC structure (controllers/models/util)
    - Add Blog Sequelize model with proper field definitions
    - Add PUT /api/blogs/:id to update likes count
    - Centralize error handling in Express middleware

---
+++ controllers/blogs.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const blog = await Blog.create({...req.body, date: new Date()})
+    return res.json(blog)
+  } catch(error) {
+    next(error)
+  }
+})
+
+const blogFinder = async (req, res, next) => {
+  try {
+    req.blog = await Blog.findByPk(req.params.id)
+    if (!req.blog) {
+      return res.status(404).end()
+    }
+    next()
+  } catch(error) {
+    next(error)
+  }
+}
+
+router.put('/:id', blogFinder, async (req, res, next) => {
+  try {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } catch(error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  await req.blog.destroy()
+  res.status(204).end()
+})
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+const blogRouter = require('./controllers/blogs')
+
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  return response.status(400).json({ error: error.message })
+}
+
+app.use(errorHandler)
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+start()
